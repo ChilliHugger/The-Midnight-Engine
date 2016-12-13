028c5d492349cff6cf72126ffa4032dd67394595
Chris Wild <chris.wild@chillihugger.com>
2016-12-13 20:45:58 +0000

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+ *  main.cpp
+ *  tme
+ *
+ *  Created by Chris Wild on 19/02/2011.
+ *  Copyright 2011 Chilli Hugger. All rights reserved.
+ *
+ */
+
+#include "main.h"
+
+int main ( void )
+{
+}
---
+++ main.h
@@ -0,0 +1,9 @@
+/*
+ *  main.h
+ *  tme
+ *
+ *  Created by Chris Wild on 19/02/2011.
+ *  Copyright 2011 Chilli Hugger. All rights reserved.
+ *
+ */
+
